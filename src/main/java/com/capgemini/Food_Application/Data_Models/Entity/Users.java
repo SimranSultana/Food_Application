7cd1156d75b39f464ebc2df654635e77495b2a0a
SimranSultana <simran07sultana@gmail.com>
2024-11-08 23:06:37 +0530

    UsersEntity modified

--- src/main/java/com/capgemini/Food_Application/Data_Models/Entity/Users.java
+++ src/main/java/com/capgemini/Food_Application/Data_Models/Entity/Users.java
@@ -1,4 +1,18 @@
 package com.capgemini.Food_Application.Data_Models.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int UserId;
+    private  String UserName;
+    private String UserEmail;
+    private String UserPlace;
 }
